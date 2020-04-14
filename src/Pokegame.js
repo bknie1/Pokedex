851f33c80549a09b77234e02ac6b9f077a1ff7d2
Brandon Knieriem <brandon@Brandons-MacBook-Air.local>
2020-04-13 22:29:13 -0700

    Added better win/lose feedback.

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -12,13 +12,19 @@ class Pokegame extends React.Component {
 		return (
 			<section className="Pokegame">
 				<h1 className="Pokegame-title">Pokemon Battle</h1>
-				<div className={determineWinner(team1, team2) == 'team 1' ? 'Pokedex-winner' : ''}>
-					<Pokedex pokemon={team1} />
-				</div>
+				{determineWinner(team1, team2) == 'team 1' ? (
+					<h4 className="Pokedex-winner">Winning Hand</h4>
+				) : (
+					<h4 className="Pokedex-loser">Losing Hand</h4>
+				)}
+				<Pokedex pokemon={team1} />
 				<h2 class="Pokegame-versus">Versus</h2>
-				<div className={determineWinner(team1, team2) == 'team 2' ? 'Pokedex-winner' : ''}>
-					<Pokedex pokemon={team2} />
-				</div>
+				{determineWinner(team1, team2) == 'team 2' ? (
+					<h4 className="Pokedex-winner">Winning Hand</h4>
+				) : (
+					<h4 className="Pokedex-loser">Losing Hand</h4>
+				)}
+				<Pokedex pokemon={team2} />
 			</section>
 		);
 	}
